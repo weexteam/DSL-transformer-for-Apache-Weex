050e206d2d6cd603b7c20a32397994b6d48dcecc
songsiqi <songsiqi2006@126.com>
2017-02-07 15:14:44 +0800

    upgrade styler version to fix warning log of non standard property name

--- test/log.js
+++ test/log.js
@@ -35,7 +35,7 @@ describe('print logs', function () {
       {name: 'foo', reason: 'ERROR: tag `tabheader` should not have children', line: 5, column: 5},
       {name: 'foo', reason: 'ERROR: tag name `text` should just have one text node only', line: 6, column: 5},
       {name: 'foo', reason: 'NOTE: unit `px` is not supported and property value `200px` is autofixed to `200`', line: 12, column: 5},
-      {name: 'foo', reason: 'WARNING: `line-height` is not a standard property name', line: 13, column: 5},
+      {name: 'foo', reason: 'WARNING: `line-height` is not a standard property name (may not be supported)', line: 13, column: 5},
       {name: 'foo', reason: 'ERROR: property value `stretch` is not supported for `justify-content` (supported values are: `flex-start`|`flex-end`|`center`|`space-between`)', line: 14, column: 5},
       {name: 'foo', reason: 'ERROR: property value `center` is not supported for `height` (only number and pixel values are supported)', line: 15, column: 5},
       {name: 'foo', reason: 'ERROR: property value `90px` is not valid for `color`', line: 16, column: 5},
@@ -43,7 +43,7 @@ describe('print logs', function () {
       {name: 'foo', reason: 'NOTE: property value `#ddd` is autofixed to `#dddddd`', line: 20, column: 5},
       {name: 'foo', reason: 'ERROR: Selector `.img .text` is not supported. Weex only support single-classname selector', line: 22, column: 3},
       {name: 'foo', reason: 'ERROR: undefined:2:10: property missing \':\'', line: 28, column: 10},
-      {name: 'foo', reason: 'ERROR: Cannot find required module "underscore"', line: 31, column: 1}])
+      {name: 'foo', reason: 'ERROR: Cannot find required module "px2rem"', line: 31, column: 1}])
   })
 
   it('one component in one line', function () {
@@ -61,7 +61,7 @@ describe('print logs', function () {
     expect(logs.length).eql(3)
     expect(logs).eql([{name: 'foo', reason: 'NOTE: property value `red` is autofixed to `#FF0000`', line: 1, column: 22},
       {name: 'foo', reason: 'NOTE: unit `px` is not supported and property value `200px` is autofixed to `200`', line: 1, column: 116},
-      {name: 'foo', reason: 'WARNING: `line-height` is not a standard property name', line: 1, column: 130}])
+      {name: 'foo', reason: 'WARNING: `line-height` is not a standard property name (may not be supported)', line: 1, column: 130}])
   })
 
   it('one component with one element', function () {
@@ -79,10 +79,10 @@ describe('print logs', function () {
     expect(logs.length).eql(6)
     expect(logs).eql([{name: 'foo', reason: 'NOTE: property value `red` is autofixed to `#FF0000`', line: 18, column: 5},
       {name: 'foo', reason: 'NOTE: unit `px` is not supported and property value `200px` is autofixed to `200`', line: 25, column: 5},
-      {name: 'foo', reason: 'WARNING: `line-height` is not a standard property name', line: 26, column: 5},
+      {name: 'foo', reason: 'WARNING: `line-height` is not a standard property name (may not be supported)', line: 26, column: 5},
       {name: 'bar', reason: 'NOTE: property value `yellow` is autofixed to `#FFFF00`', line: 4, column: 7},
       {name: 'bar', reason: 'NOTE: unit `px` is not supported and property value `100px` is autofixed to `100`', line: 10, column: 7},
-      {name: 'bar', reason: 'WARNING: `line-height` is not a standard property name', line: 11, column: 7}])
+      {name: 'bar', reason: 'WARNING: `line-height` is not a standard property name (may not be supported)', line: 11, column: 7}])
   })
 
   it('one component with multiple elements', function () {
@@ -100,13 +100,13 @@ describe('print logs', function () {
     expect(logs.length).eql(9)
     expect(logs).eql([{name: 'foo', reason: 'NOTE: property value `red` is autofixed to `#FF0000`', line: 33, column: 5},
       {name: 'foo', reason: 'NOTE: unit `px` is not supported and property value `200px` is autofixed to `200`', line: 41, column: 5},
-      {name: 'foo', reason: 'WARNING: `line-height` is not a standard property name', line: 42, column: 5},
+      {name: 'foo', reason: 'WARNING: `line-height` is not a standard property name (may not be supported)', line: 42, column: 5},
       {name: 'bar', reason: 'NOTE: property value `yellow` is autofixed to `#FFFF00`', line: 4, column: 7},
       {name: 'bar', reason: 'NOTE: unit `px` is not supported and property value `100px` is autofixed to `100`', line: 10, column: 7},
-      {name: 'bar', reason: 'WARNING: `line-height` is not a standard property name', line: 11, column: 7},
+      {name: 'bar', reason: 'WARNING: `line-height` is not a standard property name (may not be supported)', line: 11, column: 7},
       {name: 'baz', reason: 'NOTE: property value `green` is autofixed to `#008000`', line: 19, column: 7},
       {name: 'baz', reason: 'NOTE: unit `px` is not supported and property value `50px` is autofixed to `50`', line: 25, column: 7},
-      {name: 'baz', reason: 'WARNING: `line-height` is not a standard property name', line: 26, column: 7}])
+      {name: 'baz', reason: 'WARNING: `line-height` is not a standard property name (may not be supported)', line: 26, column: 7}])
   })
 
   it('no template tag', function () {
@@ -155,7 +155,7 @@ describe('log levels', function () {
       {name: 'foo', reason: 'ERROR: tag `tabheader` should not have children', line: 5, column: 5},
       {name: 'foo', reason: 'ERROR: tag name `text` should just have one text node only', line: 6, column: 5},
       {name: 'foo', reason: 'NOTE: unit `px` is not supported and property value `200px` is autofixed to `200`', line: 12, column: 5},
-      {name: 'foo', reason: 'WARNING: `line-height` is not a standard property name', line: 13, column: 5},
+      {name: 'foo', reason: 'WARNING: `line-height` is not a standard property name (may not be supported)', line: 13, column: 5},
       {name: 'foo', reason: 'ERROR: property value `stretch` is not supported for `justify-content` (supported values are: `flex-start`|`flex-end`|`center`|`space-between`)', line: 14, column: 5},
       {name: 'foo', reason: 'ERROR: property value `center` is not supported for `height` (only number and pixel values are supported)', line: 15, column: 5},
       {name: 'foo', reason: 'ERROR: property value `90px` is not valid for `color`', line: 16, column: 5},
@@ -163,7 +163,7 @@ describe('log levels', function () {
       {name: 'foo', reason: 'NOTE: property value `#ddd` is autofixed to `#dddddd`', line: 20, column: 5},
       {name: 'foo', reason: 'ERROR: Selector `.img .text` is not supported. Weex only support single-classname selector', line: 22, column: 3},
       {name: 'foo', reason: 'ERROR: undefined:2:10: property missing \':\'', line: 28, column: 10},
-      {name: 'foo', reason: 'ERROR: Cannot find required module "underscore"', line: 31, column: 1}]
+      {name: 'foo', reason: 'ERROR: Cannot find required module "px2rem"', line: 31, column: 1}]
     expect(logsNote).is.an.array
     expect(logsAll).is.an.array
     expect(logsDefault).is.an.array
@@ -186,13 +186,13 @@ describe('log levels', function () {
     expect(logs.length).eql(9)
     expect(logs).eql([{name: 'foo', reason: 'ERROR: tag `tabheader` should not have children', line: 5, column: 5},
       {name: 'foo', reason: 'ERROR: tag name `text` should just have one text node only', line: 6, column: 5},
-      {name: 'foo', reason: 'WARNING: `line-height` is not a standard property name', line: 13, column: 5},
+      {name: 'foo', reason: 'WARNING: `line-height` is not a standard property name (may not be supported)', line: 13, column: 5},
       {name: 'foo', reason: 'ERROR: property value `stretch` is not supported for `justify-content` (supported values are: `flex-start`|`flex-end`|`center`|`space-between`)', line: 14, column: 5},
       {name: 'foo', reason: 'ERROR: property value `center` is not supported for `height` (only number and pixel values are supported)', line: 15, column: 5},
       {name: 'foo', reason: 'ERROR: property value `90px` is not valid for `color`', line: 16, column: 5},
       {name: 'foo', reason: 'ERROR: Selector `.img .text` is not supported. Weex only support single-classname selector', line: 22, column: 3},
       {name: 'foo', reason: 'ERROR: undefined:2:10: property missing \':\'', line: 28, column: 10},
-      {name: 'foo', reason: 'ERROR: Cannot find required module "underscore"', line: 31, column: 1}])
+      {name: 'foo', reason: 'ERROR: Cannot find required module "px2rem"', line: 31, column: 1}])
   })
 
   it('level `ERROR`', function () {
@@ -208,7 +208,7 @@ describe('log levels', function () {
       {name: 'foo', reason: 'ERROR: property value `90px` is not valid for `color`', line: 16, column: 5},
       {name: 'foo', reason: 'ERROR: Selector `.img .text` is not supported. Weex only support single-classname selector', line: 22, column: 3},
       {name: 'foo', reason: 'ERROR: undefined:2:10: property missing \':\'', line: 28, column: 10},
-      {name: 'foo', reason: 'ERROR: Cannot find required module "underscore"', line: 31, column: 1}])
+      {name: 'foo', reason: 'ERROR: Cannot find required module "px2rem"', line: 31, column: 1}])
   })
 
   it('level `OFF`', function () {
